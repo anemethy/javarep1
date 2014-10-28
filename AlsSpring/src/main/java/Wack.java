f175c3e1800f2bf5d5e2871913644c6a89c167dd
al <al@al-desktop>
2014-10-28 12:16:14 -0400

    Change-Id: I51487af4b2ae262f540507dcf879f471b5d6d315 Change-Id: 0.01

---
+++ AlsSpring/src/main/java/Wack.java
@@ -0,0 +1,13 @@
+
+public class Wack {
+
+	public Wack() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+	  System.out.print ("Wack me !");	// TODO Auto-generated method stub
+
+	}
+
+}
